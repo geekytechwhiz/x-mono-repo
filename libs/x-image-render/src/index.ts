801fe65281af9b2b9a90d35722eceda4e0ffae5e
Kollati Murali <kollatimurali.k@hcl.com>
2024-01-31 19:06:12 +0530

    added content gallery

--- libs/x-image-render/src/index.ts
+++ libs/x-image-render/src/index.ts
@@ -1,5 +1,6 @@
+import ContentGallery from "./lib/components/ContentGallery/ContentGallery";
 import DamContentGallery from "./lib/components/damContentGallery/DamContentGallery";
 import { usePostImageCrop } from "./lib/hooks/usePostImageCrop";
 import { XImageRender } from "./lib/x-image-render";
 
-export { DamContentGallery, XImageRender, usePostImageCrop };
+export { ContentGallery, DamContentGallery, XImageRender, usePostImageCrop };
