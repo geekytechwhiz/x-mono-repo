52154fc604e602af3be904d2759c0a0cf137f973
Amala Suchith Francis Antony Beschi <amalasuchith.franc@hcl.com>
2024-02-01 23:34:39 +0530

    test app

--- apps/authoring-web/src/app/app.tsx
+++ apps/authoring-web/src/app/app.tsx
@@ -1,16 +1,8 @@
-/* eslint-disable no-debugger */
-
-import { ApolloProvider } from "@apollo/client";
 import { init as initApm } from "@elastic/apm-rum";
-import CssBaseline from "@mui/material/CssBaseline";
-import { ThemeProvider } from "@mui/material/styles";
 import { unstable_ClassNameGenerator } from "@mui/material/utils";
-import { makeStyles } from "@mui/styles";
-
 import { Suspense, useEffect, useState } from "react";
-import { I18nextProvider, useTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next";
 // import { useLocation, useNavigate } from 'react-router-dom';
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 // import { CommentProvider } from './context/CommentsContext/CommentsContext';
@@ -19,16 +11,9 @@ import "./App.css";
 // import { StoreProvider } from './store/ContextStore';
 // import LightTheme from './theme/lightTheme';
 // import { DefaultLocale } from './utils/constants';
-import { graphqlInstance } from "@platformx/authoring-apis";
-import { store } from "@platformx/authoring-state";
-import { DefaultLocale, LightTheme, getCurrentLang, getSelectedRoute } from "@platformx/utilities";
-import { Provider } from "react-redux";
-import { AnalyticsProvider } from "use-analytics";
-import AppRouter from "./router/AppRouter";
+import { DefaultLocale, getCurrentLang } from "@platformx/utilities";
 import Analytics from "./utils/analytics/analyticsData";
 import { analyticsInstance } from "./utils/analytics/dynamicAnalytics";
-import { AUTH_URL } from "./utils/authConstants";
-import { BrowserRouter } from "react-router-dom";
 
 unstable_ClassNameGenerator.configure((componentName) =>
   componentName.replace("Mui", "Platform-x-"),
@@ -48,35 +33,15 @@ initApm({
   logLevel: "debug",
 });
 
-const useStyles = makeStyles((theme) => ({
-  toastContainer: {
-    width: "457px",
-    "& .Toastify__toast": {
-      border: `2px solid #D9DBE9`,
-      "& .Toastify__toast-body": {
-        paddingLeft: 0,
-      },
-    },
-  },
-}));
-
 function App() {
   const { i18n } = useTranslation();
-  const [language, setLanguage] = useState(DefaultLocale);
-  const classes = useStyles();
-  const [instances, setInstances] = useState<any>({});
-  const routing = getSelectedRoute();
-  const { pathname } = window.location;
+  const [, setLanguage] = useState(DefaultLocale);
+  const [, setInstances] = useState<any>({});
 
   useEffect(() => {
     const initializeApp = async () => {
       try {
-        if (pathname === "/en" || pathname === "/" || pathname === `/${routing}/en`) {
-          window.location.replace(AUTH_URL);
-        }
-
         const analytics = await analyticsInstance(Analytics);
-        console.log("Analytics instance:", analytics);
         setInstances(analytics);
 
         const lang = getCurrentLang();
@@ -95,31 +60,7 @@ function App() {
   return (
     <Suspense fallback={<div>...Loading</div>}>
       <div className='App'>
-        <I18nextProvider i18n={i18n}>
-          <ApolloProvider client={graphqlInstance}>
-            {/* <AnalyticsProvider instance={instances}> */}
-            <ThemeProvider theme={LightTheme}>
-              <CssBaseline />
-              <BrowserRouter basename={routing ? `/${routing}/${language}` : `/${language}`}>
-                <Provider store={store}>
-                  <AppRouter />
-                </Provider>
-              </BrowserRouter>
-            </ThemeProvider>
-            {/* </AnalyticsProvider> */}
-            <ToastContainer
-              position='bottom-center'
-              autoClose={4000}
-              newestOnTop
-              closeOnClick
-              pauseOnFocusLoss
-              pauseOnHover
-              theme='colored'
-              icon={false}
-              className={classes.toastContainer}
-            />
-          </ApolloProvider>
-        </I18nextProvider>
+        <h1>authoring app is running fine</h1>
       </div>
     </Suspense>
   );
