c0be815142cc178741f0e8ad0392c32544ce1aa6
Neha Gupta <gupta_neha@hcl.com>
2024-02-20 15:47:16 +0530

    poll-mfe

---
+++ libs/x-poll/src/lib/x-poll.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface XPollProps {}
+
+export function XPoll() {
+  return (
+    <div>
+      <h1>Welcome to XPoll!</h1>
+    </div>
+  );
+}
+
+export default XPoll;
