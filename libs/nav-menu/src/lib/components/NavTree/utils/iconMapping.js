e3b7b6767d69bd0e9cad58f841853db3c0da5e21
Neha Gupta <gupta_neha@hcl.com>
2024-02-22 13:22:56 +0530

    eslint error resolved

--- libs/nav-menu/src/lib/components/NavTree/utils/iconMapping.js
+++ libs/nav-menu/src/lib/components/NavTree/utils/iconMapping.js
@@ -1,40 +1,40 @@
 const icon = {
   NX_NAV_ICON_1:
-    'https://dev.dam.hcl-x.com/server/api/core/bitstreams/373a4dc5-c608-404a-89ff-8b2644240f45/content',
+    "https://dev.dam.hcl-x.com/server/api/core/bitstreams/373a4dc5-c608-404a-89ff-8b2644240f45/content",
 
   NX_NAV_ICON_2:
-    'https://dev.dam.hcl-x.com/server/api/core/bitstreams/269e9b4a-41e4-444f-8e09-af7dc822b581/content',
+    "https://dev.dam.hcl-x.com/server/api/core/bitstreams/269e9b4a-41e4-444f-8e09-af7dc822b581/content",
 
   NX_NAV_ICON_3:
-    'https://dev.dam.hcl-x.com/server/api/core/bitstreams/58e0164c-29fc-4bdd-a9c5-67bc7797c3f7/content',
+    "https://dev.dam.hcl-x.com/server/api/core/bitstreams/58e0164c-29fc-4bdd-a9c5-67bc7797c3f7/content",
 
   NX_NAV_ICON_4:
-    'https://dev.dam.hcl-x.com/server/api/core/bitstreams/d387c7c3-3dae-49be-ad8e-3b4e247f6ebb/content',
+    "https://dev.dam.hcl-x.com/server/api/core/bitstreams/d387c7c3-3dae-49be-ad8e-3b4e247f6ebb/content",
 
   NX_NAV_ICON_5:
-    'https://dev.dam.hcl-x.com/server/api/core/bitstreams/6244eeaa-6df4-449d-865f-eaf80aba3ffc/content',
+    "https://dev.dam.hcl-x.com/server/api/core/bitstreams/6244eeaa-6df4-449d-865f-eaf80aba3ffc/content",
 
   NX_NAV_ICON_6:
-    'https://dev.dam.hcl-x.com/server/api/core/bitstreams/333642fb-752d-499f-aeba-481effa2caf5/content',
+    "https://dev.dam.hcl-x.com/server/api/core/bitstreams/333642fb-752d-499f-aeba-481effa2caf5/content",
 
   NX_NAV_ICON_7:
-    'https://dev.dam.hcl-x.com/server/api/core/bitstreams/f502bca9-baae-4964-a398-6fe2c4e54945/content',
+    "https://dev.dam.hcl-x.com/server/api/core/bitstreams/f502bca9-baae-4964-a398-6fe2c4e54945/content",
 
   NX_NAV_ICON_8:
-    'https://dev.dam.hcl-x.com/server/api/core/bitstreams/42085059-0636-4b57-acb1-fbc98489331d/content',
+    "https://dev.dam.hcl-x.com/server/api/core/bitstreams/42085059-0636-4b57-acb1-fbc98489331d/content",
 
   NX_NAV_ICON_9:
-    'https://dev.dam.hcl-x.com/server/api/core/bitstreams/d29e2a9c-446f-4a1e-b0a0-fc67a28f4265/content',
+    "https://dev.dam.hcl-x.com/server/api/core/bitstreams/d29e2a9c-446f-4a1e-b0a0-fc67a28f4265/content",
 
   NX_NAV_ICON_10:
-    'https://dev.dam.hcl-x.com/server/api/core/bitstreams/e0056513-65b5-45ed-a381-c1b82619f368/content',
+    "https://dev.dam.hcl-x.com/server/api/core/bitstreams/e0056513-65b5-45ed-a381-c1b82619f368/content",
 
   NX_NAV_ICON_11:
-    'https://dev.dam.hcl-x.com/server/api/core/bitstreams/048e9e07-1f04-4df9-813b-332c37f0f8aa/content',
+    "https://dev.dam.hcl-x.com/server/api/core/bitstreams/048e9e07-1f04-4df9-813b-332c37f0f8aa/content",
 
   NX_NAV_ICON_12:
-    'https://dev.dam.hcl-x.com/server/api/core/bitstreams/b5ca8fab-9a5d-4746-8441-15038ef5fbbf/content',
-}
+    "https://dev.dam.hcl-x.com/server/api/core/bitstreams/b5ca8fab-9a5d-4746-8441-15038ef5fbbf/content",
+};
 
 const iconMapping = [
   {
@@ -73,6 +73,6 @@ const iconMapping = [
   {
     url: `${icon.NX_NAV_ICON_12}`,
   },
-]
+];
 
-export default iconMapping
+export default iconMapping;
