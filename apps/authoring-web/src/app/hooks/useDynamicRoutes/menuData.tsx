2547284bcd611df2e1c8d9e854536242eefb6f85
Prasanth <prashanthmsktm@gmail.com>
2024-02-23 16:12:06 +0530

    Build issue resolved

--- apps/authoring-web/src/app/hooks/useDynamicRoutes/menuData.tsx
+++ apps/authoring-web/src/app/hooks/useDynamicRoutes/menuData.tsx
@@ -3,7 +3,7 @@ import {
   ApprovalStatus,
   ArticleIcon,
   CookieIcon,
-  CourseIcon,
+  courseIcon,
   EventsIcon,
   FeatureStarIcon,
   FooterMenuUpdated,
@@ -82,7 +82,7 @@ const ContentRoutes = {
     },
     {
       MenuName: "Courses",
-      Icon: CourseIcon,
+      Icon: courseIcon,
       url: "/content/course",
       category: "content",
       subCategory: "",
