7604dc087303f5f454259ac3fa1e383bd78f1a08
Harsh Upadhyay <harsh.upadhyay@hcl.com>
2024-02-26 16:09:38 +0530

    eslint fixes

--- libs/assets-manager/src/lib/components/CardMenu.styles.ts
+++ libs/assets-manager/src/lib/components/CardMenu.styles.ts
@@ -1,30 +1,30 @@
-import { makeStyles } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core";
 
 export const useStyles = makeStyles(() => ({
   icon: {
-    width: '16px',
-    marginRight: '10px',
+    width: "16px",
+    marginRight: "10px",
   },
-  assetnew:{
-    '&.Platform-x-Menu-paper': {
-      boxShadow: '0 3px 6px 0 rgba(0, 0, 0, 0.16)',
-      borderRadius: '7px',
-      marginTop: '5px',
+  assetnew: {
+    "&.Platform-x-Menu-paper": {
+      boxShadow: "0 3px 6px 0 rgba(0, 0, 0, 0.16)",
+      borderRadius: "7px",
+      marginTop: "5px",
     },
-    '&.Platform-x-Menu-list': {
-      borderRadius: '4px',
-      boxShadow: '0 0 2px 0 rgba(115, 114, 114, 0.14)',
-      border: 'solid 1px rgba(112, 112, 112, 0.1)',
+    "&.Platform-x-Menu-list": {
+      borderRadius: "4px",
+      boxShadow: "0 0 2px 0 rgba(115, 114, 114, 0.14)",
+      border: "solid 1px rgba(112, 112, 112, 0.1)",
     },
-    '.Platform-x-MenuItem-root': {
-      '.Platform-x-SvgIcon-root': {
+    ".Platform-x-MenuItem-root": {
+      ".Platform-x-SvgIcon-root": {
         fontSize: 20,
-        marginRight: '10px',
-        verticalAlign: 'middle',
+        marginRight: "10px",
+        verticalAlign: "middle",
       },
-      paddingLeft: '18px',
-      fontSize: '16px',
-      textTransform: 'capitalize',
+      paddingLeft: "18px",
+      fontSize: "16px",
+      textTransform: "capitalize",
     },
-  }
+  },
 }));
--- libs/sites/src/index.ts
+++ libs/sites/src/index.ts
@@ -1,4 +1,2 @@
 export { SiteListing } from "./lib/pages/SiteListing/SiteListing";
-export {AddSite} from './lib/pages/AddSite/AddSite'
-
-
+export { AddSite } from "./lib/pages/AddSite/AddSite";
