f9243c93e4127ad0d87f9834835e3425eca9cead
naveenpandi selvam <naveenpandi.selvam@hcl.com>
2024-03-01 11:18:08 +0530

    header and footer added

--- libs/x-prelems-library/src/index.ts
+++ libs/x-prelems-library/src/index.ts
@@ -3,5 +3,5 @@ import AboutUs2 from "./lib/prelems/AboutUs2/AboutUs2";
 export { AboutUs2 };
 export * from "./lib/prelems";
 export * from "./lib/ContentTypes";
-// export * from "./lib/components";
+export * from "./lib/components";
 export * from "./lib/utils";
