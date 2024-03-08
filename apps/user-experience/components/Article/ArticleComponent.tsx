67f25c17b28bca8a1e4bc16c201e8ce02f749c2e
suchith-f <amalasuchith.franc@hcl.com>
2024-03-08 18:48:08 +0530

    article test

--- apps/user-experience/components/Article/ArticleComponent.tsx
+++ apps/user-experience/components/Article/ArticleComponent.tsx
@@ -1,38 +1,39 @@
 import React from "react";
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 
-const ArticlePrelem = dynamic(
-  () => import("@platformx/x-prelems-library").then((mod) => mod.Article),
-  {
-    ssr: false,
-  },
-);
+// const ArticlePrelem = dynamic(
+//   () => import("@platformx/x-prelems-library").then((mod) => mod.Article),
+//   {
+//     ssr: false,
+//   },
+// );
 
 export const ArticleComponent = (props) => {
-  const { pageData = {}, secondaryArgs = {} } = props || {};
-  const prelemContentProp = pageData;
-  const prelemAnalyticsProp = {
-    pageId: pageData?.page,
-    pageTitle: pageData?.title,
-    pageDesc: pageData?.description,
-    pageTags: pageData?.settings?.keywords?.join(", "),
-    isAuthoring: false,
-    isSeoEnabled: pageData?.settings?.seo_blockIndexing,
-    isAnalyticsEnabled: pageData?.analytics_enable,
-  };
+  // const { pageData = {}, secondaryArgs = {} } = props || {};
+  // const prelemContentProp = pageData;
+  // const prelemAnalyticsProp = {
+  //   pageId: pageData?.page,
+  //   pageTitle: pageData?.title,
+  //   pageDesc: pageData?.description,
+  //   pageTags: pageData?.settings?.keywords?.join(", "),
+  //   isAuthoring: false,
+  //   isSeoEnabled: pageData?.settings?.seo_blockIndexing,
+  //   isAnalyticsEnabled: pageData?.analytics_enable,
+  // };
 
-  const prelemAuthoringHelper = {
-    isModalShow: true,
-  };
+  // const prelemAuthoringHelper = {
+  //   isModalShow: true,
+  // };
 
   return (
     <React.Fragment>
-      <ArticlePrelem
+      {/* <ArticlePrelem
         content={prelemContentProp}
         analytics={prelemAnalyticsProp}
         authoringHelper={prelemAuthoringHelper}
         secondaryArgs={secondaryArgs}
-      />
+      /> */}
+      <div>hello article test</div>
     </React.Fragment>
   );
 };
