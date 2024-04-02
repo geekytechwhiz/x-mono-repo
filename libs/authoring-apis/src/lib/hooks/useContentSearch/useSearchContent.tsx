20d0264b83a5e804ec31279f21ebb1cf664da66d
Prasanth M S <prasanth.murug@hcl.com>
2024-04-02 18:28:49 +0530

    Global Search  issue fixed

--- libs/authoring-apis/src/lib/hooks/useContentSearch/useSearchContent.tsx
+++ libs/authoring-apis/src/lib/hooks/useContentSearch/useSearchContent.tsx
@@ -23,6 +23,7 @@ interface UseContentSearchResult {
   error: any;
   fetchMore: () => void;
   refetch: () => void;
+  contents: any;
 }
 
 const useContentSearch = ({
@@ -40,8 +41,6 @@ const useContentSearch = ({
   });
 
   const variableCourse: any = { ...variables, filter: "Course", isListing: true };
-
-  // const variableCourse: any = { filter: "Course", isListing: true };
 
   const fetchQuery =
     contentType?.toLocaleLowerCase() === "course"
@@ -70,7 +69,7 @@ const useContentSearch = ({
       dispatch(previewContent({}));
       dispatch(updateContentList(serializableData));
     }
-  }, [data]);
+  }, [data, contents, loading, contentType, locationState, filter, startIndex]);
 
   const fetchMoreContent = async () => {
     try {
@@ -100,6 +99,7 @@ const useContentSearch = ({
   return {
     loading: isLoading,
     error,
+    contents: contents,
     fetchMore: fetchMoreContent,
     refetch: refresh,
   };
