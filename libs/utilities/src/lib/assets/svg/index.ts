ee2f0136214b5ba5e9c4de3141a3d2f8d955b1c0
Gujjala Dheeraj Chandra <gujjala.dheer@hcl.com>
2024-04-11 19:52:18 +0530

    Spaces and User Group Bug fixes

--- libs/utilities/src/lib/assets/svg/index.ts
+++ libs/utilities/src/lib/assets/svg/index.ts
@@ -60,6 +60,8 @@ import SitesIcon from "./SitesIcon.svg";
 import SpaceListIcon from "./Space.svg";
 import CardOptionUnPublishIcon from "./UnPublishIconOptionMenu.svg";
 import UsersIcon from "./UsersIcons.svg";
+import GroupUsersIcon from "./UsersIcon3.svg";
+import communitySpaceIcon from "./communities.svg";
 import CardOptionViewIcon from "./ViewIconOptionMenu.svg";
 import DraftedIcon from "./WorkflowHistory/Draft.svg";
 import EditorIcon from "./WorkflowHistory/Editor.svg";
@@ -497,4 +499,6 @@ export {
   Checkicon,
   Doticon,
   Crossnewicons,
+  GroupUsersIcon,
+  communitySpaceIcon,
 };
